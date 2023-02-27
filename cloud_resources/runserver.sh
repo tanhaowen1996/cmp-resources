480ec16f4fded1248778a5096ad9c6ad2ed72e0b
thw <tanhaowen1996@gmail.com>
2023-02-27 10:37:43 +0800

    fix bug

--- cloud_resources/runserver.sh
+++ cloud_resources/runserver.sh
@@ -9,7 +9,7 @@ WEB_CONCURRENCY=${WEB_CONCURRENCY:=3}
 MAX_REQUESTS=${MAX_REQUESTS:=5000}
 
 if [ "asgi" = "$SERVICE_MODE" ]; then
-    gunicorn cmp_storage.asgi -b :$WEB_PORT -k uvicorn.workers.UvicornWorker -w $WEB_CONCURRENCY --max-requests $MAX_REQUESTS
+    gunicorn cloud_resources.asgi -b :$WEB_PORT -k uvicorn.workers.UvicornWorker -w $WEB_CONCURRENCY --max-requests $MAX_REQUESTS
 elif [ "django" = "$SERVICE_MODE" ]; then
     ./manage.py runserver 0.0.0.0:$WEB_PORT
 else
