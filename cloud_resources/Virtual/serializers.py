8cc6c6f3de6f62e2d0d441d0abf6f438507c0518
thw <tanhaowen1996@gmail.com>
2023-03-07 14:02:04 +0800

    add volumes

--- cloud_resources/Virtual/serializers.py
+++ cloud_resources/Virtual/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Nfs, VServer
+from .models import Nfs, VServer, Volume
 
 
 class NfsSerializer(serializers.ModelSerializer):
@@ -13,4 +13,11 @@ class VServerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = VServer
+        fields = '__all__'
+
+
+class VolumeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Volume
         fields = '__all__'
