28fd03e57781ab20baa0faf5f3ede220334bc330
thw <tanhaowen1996@gmail.com>
2023-03-07 14:02:25 +0800

    add get openstack session

--- cloud_resources/Virtual/session.py
+++ cloud_resources/Virtual/session.py
@@ -1,5 +1,6 @@
 from requests.auth import HTTPBasicAuth
 from cloud_resources.settings import ONEFS_USER, ONEFS_PASSWORD
+import openstack
 
 
 class OneFSMixin:
@@ -8,3 +9,14 @@ class OneFSMixin:
     def get_session():
         nfs_session = HTTPBasicAuth(ONEFS_USER, ONEFS_PASSWORD)
         return nfs_session
+
+
+def create_connection(auth_url, region, project_name, username, password, user_domain, project_domain):
+    return openstack.connect(
+        auth_url=auth_url,
+        project_name=project_name,
+        username=username,
+        password=password,
+        region_name=region,
+        user_domain_name=user_domain,
+        project_domain_name=project_domain, )
