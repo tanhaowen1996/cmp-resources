04cde92cf7a1524ea2537a99c61eb1c6584247f7
thw <tanhaowen1996@gmail.com>
2023-03-09 14:48:11 +0800

    add nfs and volume time

--- cloud_resources/sync/syncAll.py
+++ cloud_resources/sync/syncAll.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from cloud_resources.settings import WEBHOOK_URL, SYNC_URL, SYNC_HOST_TIME, SYNC_VSERVER_TIME
+from cloud_resources.settings import WEBHOOK_URL, SYNC_URL, SYNC_HOST_TIME, SYNC_VSERVER_TIME, SYNC_NFS_TIME, SYNC_VOLUME_TIME
 import requests
 import json
 
@@ -44,9 +44,51 @@ def sync_vserver():
                       verify=False)
 
 
+def sync_nfs():
+    print("start sync nfs")
+    url = "http://{sync_url}/v2/nfs/sync_all".format(sync_url=SYNC_URL)
+    payload = {}
+    headers = {}
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print(response.text)
+        print("sync end-------")
+    except Exception as e:
+        webhook_url = WEBHOOK_URL
+        content = "文件存储同步失败, detail: {}".format(e)
+        data = {
+            "msgtype": "markdown",
+            "markdown": {"content": content}
+        }
+        requests.post(url=webhook_url, data=json.dumps(data, ensure_ascii=False).encode('utf-8'),
+                      verify=False)
+
+
+def sync_volume():
+    print("start sync nfs")
+    url = "http://{sync_url}/v2/volume/sync_all".format(sync_url=SYNC_URL)
+    payload = {}
+    headers = {}
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print(response.text)
+        print("sync end-------")
+    except Exception as e:
+        webhook_url = WEBHOOK_URL
+        content = "云硬盘同步失败, detail: {}".format(e)
+        data = {
+            "msgtype": "markdown",
+            "markdown": {"content": content}
+        }
+        requests.post(url=webhook_url, data=json.dumps(data, ensure_ascii=False).encode('utf-8'),
+                      verify=False)
+
+
 scheduler = BackgroundScheduler(timezone="Asia/Shanghai")
 
 scheduler.add_job(sync_host, 'cron', month='*', day='*', hour=int(SYNC_HOST_TIME.split(':')[0]), minute=int(SYNC_HOST_TIME.split(':')[1]), second=int(SYNC_HOST_TIME.split(':')[2]))
 scheduler.add_job(sync_vserver, 'cron', month='*', day='*', hour=int(SYNC_VSERVER_TIME.split(':')[0]), minute=int(SYNC_VSERVER_TIME.split(':')[1]), second=int(SYNC_VSERVER_TIME.split(':')[2]))
+scheduler.add_job(sync_nfs, 'cron', month='*', day='*', hour=int(SYNC_NFS_TIME.split(':')[0]), minute=int(SYNC_NFS_TIME.split(':')[1]), second=int(SYNC_NFS_TIME.split(':')[2]))
+scheduler.add_job(sync_volume, 'cron', month='*', day='*', hour=int(SYNC_VOLUME_TIME.split(':')[0]), minute=int(SYNC_VOLUME_TIME.split(':')[1]), second=int(SYNC_VOLUME_TIME.split(':')[2]))
 
 scheduler.start()
